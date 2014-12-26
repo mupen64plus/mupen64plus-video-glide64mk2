mupen64plus/mupen64plus-video-glide64mk2

commit 339ca50a57364b2509a6b5e7a073698038096a4d

clang doesnt have the to_wstring method

src/GlideHQ/TxDbg.cpp
@@ -53,7 +53,7 @@ TxDbg::~TxDbg()
 void
 TxDbg::output(const int level, const wchar_t *format, ...)
 {
-#ifdef _GLIBCXX_HAVE_BROKEN_VSWPRINTF
+#if defined(_GLIBCXX_HAVE_BROKEN_VSWPRINTF) || defined(__clang__)
   wchar_t newformat[4095];
 #else
   std::wstring newformat;
@@ -65,8 +65,8 @@ TxDbg::output(const int level, const wchar_t *format, ...)
     return;
 
   va_start(args, format);
-#ifdef _GLIBCXX_HAVE_BROKEN_VSWPRINTF
-  swprintf(newformat, L"%d:\t", level);
+#if defined(_GLIBCXX_HAVE_BROKEN_VSWPRINTF) || defined(__clang__)
+  swprintf(newformat, 4095, L"%d:\t", level);
   wcscat(newformat, format);
   vfwprintf(_dbgfile, newformat, args);
 #else

commit ed672e30c341507f21841a32de8d2cf89a7787ac

Fix swprintf call for _GLIBCXX_HAVE_BROKEN_VSWPRINTF
339ca50a57364b2509a6b5e7a073698038096a4d broke build with GCC for Windows for
code which was specific to windows. This should have never been mixed with
clang specific hacks.

Just split this call in a clang and a _GLIBCXX_HAVE_BROKEN_VSWPRINTF specific
implementation.

src/GlideHQ/TxDbg.cpp
@@ -66,7 +66,11 @@ TxDbg::output(const int level, const wchar_t *format, ...)
 
   va_start(args, format);
 #if defined(_GLIBCXX_HAVE_BROKEN_VSWPRINTF) || defined(__clang__)
+  #if defined(_GLIBCXX_HAVE_BROKEN_VSWPRINTF)
+  swprintf(newformat, L"%d:\t", level);
+  #else
   swprintf(newformat, 4095, L"%d:\t", level);
+  #endif
   wcscat(newformat, format);
   vfwprintf(_dbgfile, newformat, args);
 #else