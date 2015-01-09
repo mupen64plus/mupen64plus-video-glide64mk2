384f398dcc15c7dc02eefd48be59ce30e85fafb2
littleguy77 <mattlichter@live.com>
2015-01-08 22:36:44 -0500

    Fix name collision when LOGGING defined.
    Name out_buf collides in Glide64/rdp.cpp and Glitch64/*glitchmain.cpp.

--- src/Glitch64/OGLglitchmain.cpp
+++ src/Glitch64/OGLglitchmain.cpp
@@ -250,7 +250,7 @@ void display_error()
 #endif // _WIN32
 
 #ifdef LOGGING
-char out_buf[256];
+char log_buf[256];
 bool log_open = false;
 std::ofstream log_file;
 
@@ -281,8 +281,8 @@ void LOG(const char *text, ...)
     return;
 	va_list ap;
 	va_start(ap, text);
-	vsprintf(out_buf, text, ap);
-  log_file << out_buf;
+	vsprintf(log_buf, text, ap);
+  log_file << log_buf;
   log_file.flush();
 	va_end(ap);
 }
